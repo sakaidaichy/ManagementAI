5652b48bc314465333a4fc3aac6a0560f55f9a46
酒井大地 <sakai@yunosato.co.jp>
2026-06-30 15:46:33 +0900

    ニュース検索画面を追加

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from web.routes.dashboard import dashboard_bp
+from web.routes.news import news_bp
 
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(dashboard_bp)
+    app.register_blueprint(news_bp)
 
     return app
